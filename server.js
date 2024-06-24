d1539c98eed1bc6d6a5862e98b67978da8398331
Alan Gonzalez <alan.gonzalez87@hotmail.com>
2024-06-24 15:31:25 -0300

    week1

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "layouts/layout"); 
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
+// index route
+
+app.get('/', (req, res) => {
+  res.render('index', {title: 'home'}); // Asegúrate de tener un archivo `index.ejs` en la carpeta `views`
+});
+
+app.use(static);
+
+// Información del servidor
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
+
+// Inicio del servidor
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(`Something broke! Error: ${err.message}`);
+});
