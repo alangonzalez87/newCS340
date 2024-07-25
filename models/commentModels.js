29def6b71fb6641915b84f632797b1df9cd3e040
Alan Gonzalez <alan.gonzalez87@hotmail.com>
2024-07-25 19:56:28 -0300

    fix route

--- models/commentModels.js
+++ models/commentModels.js
@@ -1,7 +1,7 @@
 const pool = require('../database/');
 
 
-// Agregar un nuevo comentario
+
 const addComment = async (commentText, accountId, parentCommentId = null) => {
     const query = 'INSERT INTO comments (comment_text, account_id, parent_comment_id) VALUES ($1, $2, $3) RETURNING *';
     const values = [commentText, accountId, parentCommentId];
@@ -9,7 +9,7 @@ const addComment = async (commentText, accountId, parentCommentId = null) => {
     return result.rows[0];
 };
 
-// Obtener todos los comentarios y sus respuestas
+
 const getComments = async () => {
     const query = `
         SELECT * FROM comments
@@ -26,7 +26,7 @@ const getComments = async () => {
     return comments;
 };
 
-// Obtener respuestas para un comentario específico
+
 const getResponses = async (parentCommentId) => {
     const query = 'SELECT * FROM comments WHERE parent_comment_id = $1 ORDER BY created_at DESC';
     const result = await pool.query(query, [parentCommentId]);
