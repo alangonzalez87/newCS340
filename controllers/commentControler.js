4689d0e9b6531a0e42123f8b92a4069fc6d30214
Alan Gonzalez <alan.gonzalez87@hotmail.com>
2024-07-27 16:24:19 -0300

    css apply

--- controllers/commentControler.js
+++ controllers/commentControler.js
@@ -9,14 +9,35 @@ commentController.getComments = async (req, res) => {
     try {
         console.log('Fetching comments...');
         const comments = await commentModel.getComments();
+        const highlightedCommentId = comments.length > 0 ? comments[0].comment_id : null;
+        const commentsWithHighlight = comments.map(comment => ({
+            ...comment,
+            is_current: comment.comment_id === highlightedCommentId
+        }));
         console.log('Comments fetched:', comments);
         console.log('Rendering comments page...');
-        res.render('comment/comments', { comments, title: 'Comments', });
+        res.render('comment/comments', { comments: commentsWithHighlight, title: 'Comments', });
     } catch (error) {
         console.error('Error getting comments:', error);
         res.status(500).send('Server error');
     }
 };
+// invCont.getComments = async (req, res) => {
+//     const comments = await getCommentsFromDatabase(); // Función que obtiene comentarios
+
+//     // Lógica para determinar el comentario destacado (por ejemplo, el más reciente)
+//     const highlightedCommentId = comments.length > 0 ? comments[0].comment_id : null; // Ejemplo: destacar el primer comentario
+
+//     // Añadir un atributo is_current a los comentarios
+//     const commentsWithHighlight = comments.map(comment => ({
+//         ...comment,
+//         is_current: comment.comment_id === highlightedCommentId
+//     }));
+
+//     res.render('comments', { comments: commentsWithHighlight });
+// };
+
+
 
 
 commentController.addComment = async (req, res) => {
